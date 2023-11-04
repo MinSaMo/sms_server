7146e0362b6269f2093d3f04c63de71aea7f02dd
jaemin <anjm1020@gmail.com>
2023-11-04 17:39:19 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/com/sevendwarfs/sms/SmsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sevendwarfs.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SmsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
