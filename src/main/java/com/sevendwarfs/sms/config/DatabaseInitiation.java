3a78f8d4deef107ddd6370b2e8dd98bfac021d7e
jaemin <anjm1020@gmail.com>
2023-11-06 19:15:02 +0900

    test: test용 DB초기화 구현 (#1)

---
+++ src/main/java/com/sevendwarfs/sms/config/DatabaseInitiation.java
@@ -0,0 +1,27 @@
+package com.sevendwarfs.sms.config;
+
+import com.sevendwarfs.sms.domain.Member;
+import com.sevendwarfs.sms.domain.MemberRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseInitiation {
+
+  private final MemberRepository memberRepository;
+
+  @PostConstruct
+  private void init() {
+    Member saved = memberRepository.save(Member.builder()
+        .age(-1)
+        .name("assistant")
+        .build());
+
+    log.info("[DB INIT] system member created : {}", saved);
+  }
+
+}
