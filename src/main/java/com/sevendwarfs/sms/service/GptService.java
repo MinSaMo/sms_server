f4054212a5bb7fa273cc22a03c708ba955081989
jaemin <anjm1020@gmail.com>
2023-11-09 16:30:22 +0900

    fix: @Component에서 @Service으로 애노테이션 변경 (#7)

--- src/main/java/com/sevendwarfs/sms/service/GptService.java
+++ src/main/java/com/sevendwarfs/sms/service/GptService.java
@@ -12,9 +12,9 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Slf4j
 public class GptService {
 
