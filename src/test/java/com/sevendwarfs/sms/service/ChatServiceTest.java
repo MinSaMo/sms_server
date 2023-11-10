7b7d27310bfa078d3df8c807634c2bc8d51b8397
jaemin <anjm1020@gmail.com>
2023-11-10 19:01:32 +0900

    test: messageService 테스트 추가 (#16)
    - 발화 의도 테스트 메소드 이름 변경
    - 이상발화 판단 테스트 추가

--- src/test/java/com/sevendwarfs/sms/service/ChatServiceTest.java
+++ src/test/java/com/sevendwarfs/sms/service/ChatServiceTest.java
@@ -1,12 +1,19 @@
 package com.sevendwarfs.sms.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.sevendwarfs.sms.domain.OddMessage;
+import com.sevendwarfs.sms.domain.OddMessageRepository;
 import com.sevendwarfs.sms.service.enums.MessageClassification;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -14,13 +21,29 @@ class ChatServiceTest {
 
   @Autowired
   ChatService chatService;
+  @Autowired
+  MessageService messageService;
+  @Autowired
+  OddMessageRepository oddMessageRepository;
 
+  @BeforeEach
+  void beforeEach() {
+    oddMessageRepository.deleteAll();
+  }
   @Test
-  void 발화_의도_테스트() {
+  void 발화_의도_테스트_NOT_SUPPORTED() {
     String message = "Can you see the pattern on this dollar bill?";
     MessageClassification classification = chatService.classifyMessage(message);
 
-    assertEquals(classification,MessageClassification.NOT_SUPPORTED);
+    assertEquals(classification, MessageClassification.NOT_SUPPORTED);
+  }
+
+  @Test
+  void 발화_의도_테스트_SUPPORTED() {
+    String message = "Hello";
+    MessageClassification classification = chatService.classifyMessage(message);
+
+    assertEquals(classification, MessageClassification.SUPPORTED);
   }
 
   @Test
@@ -28,5 +51,34 @@ class ChatServiceTest {
 
     String message = "매운 음식 추천해줘";
     String reply = chatService.replyToMessage(message);
+  }
+
+  @Test
+  @Transactional
+  void 이상발화_판단_테스트_정상케이스() {
+    String script = "안녕";
+    Long messageId = messageService.createUserMessage(script);
+
+    Optional<Long> optionalOdd = chatService.recognizeMessage(messageId);
+    assertTrue(optionalOdd.isEmpty());
+  }
+
+  @Test
+  @Transactional
+  void 이상발화_판단_테스트_이상발화_케이스() {
+    String script = "달그락탈그락 우우을탈락라라락";
+    Long messageId = messageService.createUserMessage(script);
+
+    Optional<Long> optionalOdd = chatService.recognizeMessage(messageId);
+    assertTrue(optionalOdd.isPresent());
+    Long oddId = optionalOdd.get();
+
+    Optional<OddMessage> optionalOddMessage = oddMessageRepository.findById(oddId);
+    assertTrue(optionalOddMessage.isPresent());
+    OddMessage oddMessage = optionalOddMessage.get();
+    boolean isOdd = oddMessage.getIsDisorganized() || oddMessage.getIsDelusions() ||
+        oddMessage.getIsHallucination() || oddMessage.getIsLinguisticDerailment();
+    assertTrue(isOdd);
+    assertFalse(oddMessage.getReason().isEmpty());
   }
 }
