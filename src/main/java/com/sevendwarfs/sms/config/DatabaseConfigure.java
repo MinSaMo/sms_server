e27997dc8975d6bff45ff24d7307e53109fc52f2
jaemin <anjm1020@gmail.com>
2023-11-10 17:37:22 +0900

    fix: db init 삭제

--- src/main/java/com/sevendwarfs/sms/config/DatabaseConfigure.java
+++ src/main/java/com/sevendwarfs/sms/config/DatabaseConfigure.java
@@ -1,8 +1,5 @@
 package com.sevendwarfs.sms.config;
 
-import com.sevendwarfs.sms.domain.Member;
-import com.sevendwarfs.sms.domain.MemberRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -11,17 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class DatabaseConfigure {
-
-  private final MemberRepository memberRepository;
-
-  @PostConstruct
-  private void init() {
-    Member saved = memberRepository.save(Member.builder()
-        .age(-1)
-        .name("assistant")
-        .build());
-
-    log.info("[DB INIT] system member created : {}", saved);
-  }
-
 }
